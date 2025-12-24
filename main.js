2797dbc8de9719e0798ad22b8b09bf49f0c4ab5f
SHOKHCODERN1 <zokirovshohjahon12@gmail.com>
2025-12-24 21:07:08 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,143 @@
+const loginBox = document.getElementById("loginBox");
+const registerBox = document.getElementById("registerBox");
+const mainBox = document.getElementById("mainBox");
+const asosiy = document.getElementById("asosiy_menyu");
+const asosi = document.getElementById("asosiy");
+const imghidden = document.getElementById('img_hidden')
+const davlatlarBox = document.getElementById("davlatlarBox");
+
+const davlatlar = [
+    { name: "Uzbekistan", info: `Oʻzbekiston (rasman: Oʻzbekiston Respublikasi) — Markaziy Osiyoning markaziy qismida joylashgan mamlakat. Poytaxti — Toshkent shahri. Davlat tili — oʻzbek tili. Maydoni — 448 978 km2[7]. Hozirda mamlakatning umumiy aholisi soni 38 107 166 kishi [8]. Pul birligi — soʻm. Oʻzbekiston Respublikasi hududi 12 ta viloyat, Toshkent shahri va Qoraqalpogʻiston Respublikasidan iboratdir, shuningdek, u mustaqil, demokratik, dunyoviy va konstitutsiyaviy davlat hisoblanadi. Ammo, nodavlat tashkilotlar Oʻzbekistonni „fuqarolik huquqlari cheklangan avtoritar davlat“ deb taʼriflaganlar[9]. Oʻzbekiston MDH, BMT, YXHT va SHHT aʼzosidir. Oʻzbekiston berk hududda yaʼni qirgʻoqqa ega boʻlmagan besh mamlakat bilan, yaʼni: shimoldan Qozogʻiston; shimoli-sharqdan Qirgʻiziston; janubi-sharqdan Tojikiston; janubdan Afgʻoniston; va janubi-gʻarbiy qismida Turkmaniston bilan chegaradosh.
+
+Oʻzbekiston iqtisodiyoti bozor iqtisodiyotiga bosqichma-bosqich oʻtadi, tashqi savdo siyosati import oʻrnini bosishga asoslangan. 2017-yil sentyabridan boshlab mamlakat valyutasi bozor kursi boʻyicha toʻliq konvertatsiya qilinmoqda. Oʻzbekiston paxta tolasini ishlab chiqaruvchi va eksport qiluvchi yirik taʼminotchi davlatdir. Mamlakatda, shuningdek, dunyodagi eng yirik oltin konlari mavjud. Sovet davridagi ulkan energiya ishlab chiqarish qurilmalari va tabiiy gazni yetkazib berish bilan Oʻzbekiston Markaziy Osiyodagi eng yirik elektr ishlab chiqaruvchisi boʻldi.
+
+Oʻzbek soʻzining kelib chiqishi
+Oʻzbek etnonimining kelib chiqishi toʻgʻrisida bir nechta tahminlar mavjud. Shaxsiy ism sifatida oʻzbek soʻzining birinchi eslatilishi XII asrga toʻgʻri keladi. Oʻzbek soʻzi turkiy qabilalari orasida Movarounnahrda XIII asrda moʻgʻullar kelguniga qadar paydo boʻlgan. XII asr arab tarixchisi Usoma Ibn Munqiz „Taʼlim kitobida“ Saljuqiylar davrida 1115–1116-yildagi yoʻlboshlovchilaridan biri amir Oʻzbek degan Mosul hukmdori boʻlganligini taʼkidlaydi[10]. Tarixchi Rashiduddinning yozishicha, Tabrizda hukmronlik qilgan Ildegiziylar sulolasining soʻnggi vakili Oʻzbek Muzaffar (1210–1225) boʻlgan[11], 1221-yilda Afgʻonistondagi Xorazmshoh Jaloliddin qoʻshinlarining yetakchilaridan biri Jahon Paxlavon Oʻzbek Tayi edi[12].
+
+Tarixchi Mutal Ermatov: „Oʻzbek soʻzi „Oʻzlar“ urugʻi nomidan kelib chiqqan“, — deb taxmin qilgan[13]. Tarix fanlari doktori, professor R. G. Mukminova oʻzbeklarning etnik ismini Oʻzbek Xon nomi bilan bogʻlashga qarshi qoʻshimcha dalillar keltirgan. Sharofiddin Ali Yazdiy Oʻzbek Xon davridan ancha oldin 1289-yilgi voqealar haqida xabar berib, Tabriz viloyatidagi oʻzbek qoʻshinlarining reydini eslatib oʻtadi[14].
+
+Boshqa tarixchilarning fikricha, XIV asrning 60-yillarida, Dashti qipchoq hududida oʻzbeklar deb nomlangan koʻchmanchi turkiy qabilalarining ittifoqi tuzildi. XIV asrning 60-yillarida „oʻzbek“ etnonimi sharqiy Dashti qipchoqning butun turkiy aholisi uchun jamoaviy nomga aylandi. „Oʻzbek“ etnonimi Amir Temur davrida Movarounnahrga kirib kelgan. Alisher Navoiy (1441–1501) yozgan asarlarida „oʻzbek“ etnonimini Xuroson va Movarounnahrda tilga olgan[15].
+
+Tarixi
+Asosiy maqola: Oʻzbekiston tarixi
+Bugungi kunda Oʻzbekiston nomi bilan tanilgan hudud oldin Movarounnahr, Soʻgʻdiyona hamda Buxoro xonligi nomlari bilan ham tanilgan. XIV asrda ushbu mintaqada Amir Temur dunyoga kelgan va oʻz saltanatining markaziy qismiga aylantirgan. Amir Temur davrida ushbu hudud Qora dengizdan Arabiston dengiziga va Hindistonning Dehli shahriga qadar choʻzilgan imperiyaning poytaxtiga aylangan.
+
+Markaziy Osiyoda birinchi boʻlib oʻrnashgan odamlar Skiflar boʻlib, ular bugungi Oʻzbekiston hududining shimolida joylashgan dashtlardan tahminan miloddan avvalgi birinchi mingyillikda koʻchib kelishgan. Ushbu koʻchmanchilar hududga kelib, daryolar vodiylarida keng qamrovli sugʻorish tizimlarini barpo etishgan[16]. Aynan shu davrdan boshlab Buxoro va Samarqand kabi shaharlar maʼmuriy markazlar va madaniyat oʻchoqlari sifatida paydo boʻla boshladi[16]. Miloddan avvalgi V asrga kelib hudud Baqtriya, Soʻgʻd va Toxariston davlatlari qoʻli ostida edi[16].
+
+Sharqiy Osiyo va Gʻarb orasida savdo aloqalari rivojlanar ekan, Movarounnahr hududidagi shaharlar va qishloqlar ushbu savdo aloqalarida muhim ahamiyat kasb eta boshladi va ushbu savdo munosabatlarida badavlat soʻgʻd savdogarlarining roli katta boʻlgan. Keyinchalik Buyuk Ipak yoʻli nomi bilan tanilgan ushbu savdo yoʻlida joylashgan Buxoro va Samarqand kabi shaharlar oxir-oqibatda oʻta badavlat shaharlarga aylandi va Movarounnahrning oʻzi esa Antik davrning eng taʼsirli va qudratli hududlaridan biriga aylandi[16].` },
+    { name: "Portugal", info: `Portugaliya (portugalcha: Portugal), Portugaliya Respublikasi (portugalcha: República Portuguesa) – Yevropaning janubiy -gʻarbida, Pireney yarim orolida joylashgan davlat. Atlantika okeanidagi Azor orollari va Madeyra arxipelagini oʻz ichiga oladi. Maydoni - 92,3 ming km². Aholisi - 10,53 mln. kishi (2012). Poytaxti – Lissabon shahr. Maʼmuriy jihatdan 2 muxtor viloyat, 22 okrug (distrito)ra boʻlingan, shundan 18 tasi materikda joylashgan boʻlib, 11 tarixiy viloyatga birlashgan.
+
+Davlat tuzumi
+Portugaliya – respublika. Amaldagi konstitutsiyasi 1976-yil 25-aprelda kuchga kirgan, unga 1982 va 1989-yillarda oʻzgartirishlar kiritilgan. Davlat boshligʻi – prezident (1996-yildan Jorje Sampayyu), u umumiy toʻgʻri va yashirin ovoz berish yoʻli bilan 5 yilga va kupi bilan ikki muddatga saylanadi. Qonun chiqaruvchi hokimiyatni Respublika majlisi (bir pala-tali parlament), ijrochi hokimiyatni Vazirlar Kengashi (hukumat) amalga oshiradi.
+
+Tabiati
+Atlantika okeani sohillari payettekislik, qumli, kam parchalangan. Shimolida Meseta yassi togʻligining chekka qismlari joylashgan. Esh-trela togʻi – mamlakatning eng baland nuqtasi (1991 m). Gʻarbida togʻlar dengiz boʻyi pasttekisligiga tik tushadi. Teju daryosi janubida Portugaliya past-tekisligi bor. Eng chekka jan.ni Ser-ra-daalgarvi past togʻlari egallagan. Volfram, qalay, mis, temir, qoʻrgʻoshin, uran rudalari, kumir, kaolin, berill, marmar, oltin, kumush konlari topilgan. Iqlimi – subtropik. Oʻrta dengizga xos. Yanvarning oʻrtacha temperaturasi sohilda 10°, ichki hududlarda 8° gacha, iyulniki – sohilda 20° va ichki hududlarda 27°. Yogʻingarchilik sohilda yiliga 1000 mm, togʻlarda 2000 mm. Doru (Duero), Teju va Gvadiana daryolarida kema qatnaydi. Shimolida togʻ-oʻrmon podzol, markaziy va janubiy hududlarda qoʻngʻir va jigarrang, dare vo-diylarida allyuvial tuproklar. Sohilda botqoq yerlar, shoʻrxok tuproklar koʻp. Mamlakat janubiy oʻsimliklari Oʻrta dengiz atrofiga xos, togʻlar makvis chakalakzorlari bilan qoplangan, shimolidagi togʻlarda tosh eman va poʻkak eman, kashtan, sohilda qaragʻayzorlar uchraydi. Togʻlardagi kattagina maydonlar oʻtloq. Hayvonot dunyosi boʻri, tulki, suvsar, genetta, tipratikan, yovvoyi quyon va hokazolardan iborat. Kemiruvchilar, koʻrshapalaklar, har xil parranda koʻp. Jeresh va Ria Formoza milliy bogʻlari, bir necha qoʻriqxonalar bor.
+
+Aholisining 99% – portugallar; ispanlar va boshqalarlar ham yashaydi. Rasmiy tillar – portugal va mirandes tillari. Shahar aholisi 35% ga yaqin. Dindorlari – katoliklar. Yirik shaharlari: Lissabon, Portu, Amadora.
+
+Tarixi
+P. hududida paleolit davrida manzilgoxlar boʻlgan. Miloddan avvalgi 4—3-asrlardan bu yerda luzitanlar yashagan. Miloddan avvalgi 1-asr oxirida Qad. Rim ixtiyoriga utib, uning Luzitaniya viloyati boʻlib qoldi. Milodiy 5-asr 1yarmidan svevlar, 6-asr 2yar-midan vestgotlar hukmronlik qildi. 713—18 yillarda P. hududini arablar va barbar (mavr)lar bosib olib, Arab xalifaligi tarkibiga qushdi. Ozodlik kurashi (Rekonkista) natijasida mamlakat 1249—50 yillarda arablar istib-dodidan butunlay qutuldi, portugal millati tarkib topdi, Portugaliya davlati vujudga keldi. P.ning geografik oʻrni qulay boʻlganligi tufayli savdosotiq va hunarmandchilik rivojlandi, shaharlar tez usdi. Qirollik hokimiyati kuchayib, uzga mamlakatlarni bosib ola boshladi. Afrika va Osiyodagi bir qancha hududlarni mustamlakaga aylantirdi. 1500-yildan Braziliyani egallashga kirishdi. 16-asrning 1yarmi P. imperiyasi ravnaq topgan davr buldi. Ammo juda katta boylikni qoʻlga kiritib olgan maranlar (xristianlikni qabul qilgan yahudiylar)ning chet ellarga ketib qolishi sababli P. iqtisodiyoti tushkunlikka uchray boshladi. 1581—1640-yillarda P. Ispaniya hukmronligida buldi. 18-asr boshida P. iqtisodiy va siyosiy jihatdan Angliyaga butunlay karam bulib qoldi. 1750-yildan mamlakatda „maʼrifiy absolyutizm“ ruhidagi islohotlar tufayli bir muncha iqtisodiy va siyosiy jonlanish boshlangan bulsa ham, 1777-yildan key-in bu isloxrtlar bekor qilindi. 1807-yilda Napoleon 1 qoʻshinlari P.ni bosib oldi. 1811-yilda fransuz bosqinchilari haydab chiqarildi, biroq mamlakat 1820-yilgacha ingliz bosqinchilari zulmi ostida qoldi. 1820-yil P. inqilo-bidan soʻng islohotlar utkazildi. 1821-yil taʼsis korteslari qabul qilgan liberal konstitutsiya zamindorlarning imtiyozlari va inkvizitsiyani bekor qildi. Sanoat korxonalari, jumladan, toʻqimachilik fabikalari barpo etildi, 1853-yildat.y. ishga tushirildi. Ammo P. 20-asr boshlarida ham qoloq mamlakatligicha qolaverdi.` },
+    { name: "Brazil", info: "Braziliya, rasmiy nomi Braziliya Federativ Respublikasi (portugalcha: Brasil yoki República Federativa do Brasil) – Janubiy Amerikadagi eng katta va aholisi eng koʻp boʻlgan mamlakat boʻlib, ham aholi soni, ham maydoni jihatidan dunyoda beshinchi oʻrinni egallaydi. Maydoni – 8 512 760 ming km2. Aholisi – 212 411 400 kishi (2025). U Janubiy Amerika markazidan to Atlantika okeaniga qadar yoyilgan hududni egallab Amerikalarning „eng sharqiy“ mamlakatidir. U bilan chegaradosh mamlakatlar: Urugvay, Argentina, Paragvay, Boliviya, Peru, Kolumbiya, Venesuela, Guyana, Surinam va Fransuz Giyanasining fransuz qismi. Aniqrogʻi, u Ekvador va Chilidan tashqari, Janubiy Amerikaning har bir davlati bilan chegaradosh. Nomi, dastlabki kolonistlar tomonidan juda qadrlangan, Braziliya daraxtidan (pau-brasil) kelib chiqqan. Hududida ham ekin maydonlari, ham tropik oʻrmonzorlar juda katta joyni egallaydi. Tabiiy resurslar va katta miqdordagi ishchi kuchiga boy boʻlgan Braziliya, Janubiy Amerika iqtisodiyotining yetakchisidir. Dastlab Portugaliyaning sobiq koloniyasi boʻlganidan, uning davlat tili Portugal tilidir. Poytaxti – Brazilia shahri. BMT aʼzosi. Maʼmuriy jihatdan 26 shtat va federal (poytaxt) okrugga boʻlinadi." },
+    { name: "Argentina", info: `Argentina Respublikasi (Ispancha: República Argentina) – Janubiy Amerikadagi davlat. Qitʼaning janubi-sharqiy qismini, Olovli Yer o.ning sharqiy qismini va bir qancha orollarni egallaydi. Atlantika okeani sohilida joylashgan. Maʼmuriy jihatdan 22 viloyat (provinsiya), federal (poytaxt) okrugi va Olovli Yer hududiga boʻlinadi. Maydoni 2767 ming km². Aholisi 36,1 mln. 605kishi (1999). Poytaxti Buenos-Ayres shahri.
+
+Davlat tuzumi
+Argentina – federativ respublika. Amaldagi konstitutsiyasi 1994-yil 24-avgustda qabul qilingan. Davlat boshligʻi – prezident. Prezident va vise-prezident 4 yil muddatga saylanadi. Qonun chiqaruvchi hokimiyat – Millim kongress (parlament). Kongress ikki palata: senat va deputatlar palatasidan iborat. Ijrochi hokimiyat prezident va hukumatdir.
+
+Tabiati
+Argentinaning sharqi keng tekisli-klardan va gʻarbi togʻlardan iborat. Mamlakatning butun shimoli-sharqini La-Plata pasttekisligi (Gran-Chako, Sharqiy Pampa tekisligi), janubi-sharqini Patagoniya yassi togligi egallaydi. Gʻarbdagi togʻ yon bagʻirlari balandlasha borib, And toglariga ulanib ketadi. Janubiy Amerikaning eng baland nuqtasi Akonkagua (6960 m) ham Argentina hududidadir. Foydali qazilmalari: neft, gaz, temir, poli-metallar, uran, mis, qalay, volfram va boshqa rudalar. Argentina tekislik qismining iqlimi – tropik va subtropik, janubida – moʻ’tadil. Eng issiq oy (yanvar)ning oʻrtacha harorati shimolida 28°, janubida 10°, iyulning oʻrtacha harorati shimolida 18° va janubida G. Togʻlarda iqlim moʻ’tadil sovuq. Yiliga Patagoniyada 100 – 300 mm, shimoli-sharqda 1400 – 1600 mm, Andning sharqiy yon bagʻirlarida 5000 mm gacha yogʻin yogʻadi. Asosiy daryolari: Parana, Paragvay, Urugʻvay, Rio-Sala-do, Rio-Kolorado, Chubut, Rio-Negro. Argentina koʻllarining aksariyati muzliklardan paydo boʻlgan (Naul-Uapi, Buenos-Ay-res, Vedma va boshqalar). Argentina hudu-dining salkam 20% doimiy koʻm-koʻk va bargi toʻkiluvchi oʻrmonlar bilan qoplangan. Gran-Chakodagi qizil tuproqda siyrak oʻrmon, Pampadagi unumdor qoratuproqda pichanzor dashtlar, Patagoniyada -yarim choʻl. Argentinaning milliy bogʻlari – Iguasu, Lanin, Na-uel-Uapi va boshqalar.
+
+Aholisi
+Argentinlar (mamlakat aholisining 90%) asosan yevropalik muhojirlardan shakllangan. Shuningdek italyanlar, yahudiylar, ukrainlar, ispanlar, katalonlar, polyaklar, nemislar, fransuzlar va boshqalarlar ham yashaydi. Tub joy aholisi – indeyslarning aksariyati yevropaliklarning mustamlakachilik harakatlari jarayonida qirilib ketgan. Ularning oz qismi (kechua, tupi-guaranlar va boshqalar) Argentinaning shimoli-gʻarbida va Paragvay bilan boʻlgan chegara yaqinida yashaydi. Rasmiy tili – ispan tili. Dindorlarning aksariyati katoliklar. Aholining oʻrtacha zichligi 1 km²ga 9 kishidan koʻproq. Aholining 86% shaharlarda yashaydi. Yirik shaharlari: Buenos-Ayres, Kordova, Rosario, La-Plata.` },
+    { name: "France", info: `Fransiya yoki Farangiston (fransuzcha: France [fʁɑ̃s] tingla), rasmiy nomi Fransuz Respublikasi (fransuzcha: République française)[5] – Gʻarbiy Yevropadagi davlat. Gʻarbda va shimolda Atlantika okeani hamda La-Mansh boʻgʻozi, janubida Oʻrta dengiz bilan oʻralgan. Maydoni - 674,687 km². Aholisi - 66 561 000 kishi (2024). Poytaxti – Parij shahri.
+
+Pul birligi - Yevro (EUR) Parij eng katta shahri va poytaxti hisoblanadi. Maʼmuriy jihatdan 22 region (viloyat), 96 departamentga boʻlingan. Fransiya tarkibida dengiz orti departamentlari (Gvadelupa, Martinika, Gviana, Reyunon), dengiz orti hududlari (Taiti, Yangi Kaledoniya, Fransuz Polineziyasi, Tinch okeandagi Uollis va Futuna orollari va boshqalar), hududiy birliklar (Mayotta va Sen-Pyer va Mikelon) bor.
+
+Davlat tuzumi
+Fransiya – respublika, tarixda xonlik bo‘lgan. Amaldagi konstitutsiyasi 1958-yil referendumda qabul qilingan (bir necha marta oʻzgartishlar kiritilgan). Davlat boshligʻi – prezident. U umumiy va toʻgʻridan toʻgʻri ovoz berish yoʻli bilan 5 yil muddatga saylanadi va yana 1 marta saylanishi mumkin. Qonun chiqaruvchi hokimiyat – parlament 2 palata (Milliy majlis va Senat)dan iborat. Ijrochi hokimiyatni prezident va Vazirlar Kengashi (hukumat) amalga oshiradi.
+
+Tabiati
+
+Fransuz Polineziyasidagi Bora Bora
+Fransiyaning sharq va janubida togʻ tizmalari, markazining janubiy qismida togʻ va keng tekisliklar bor. Alp togʻlari bilan Yura togʻlari Italiya va Shveysariya bilan tabiiy chegarani tashkil etgan. Fransiyadagi Alp togʻlarining koʻp choʻqqilari 4000 m dan ziyod. Eng baland joyi – Monblan togʻi (4807 m). Balandligi 1700 m gacha boʻlgan Yura togʻlari Belfort pastligi tomonga Rona daryosi vodiylari uzra pasayib boradi. Belfort pastligining shimoli- sharqiy tomonidan Reyn boʻylab, Fransiya Germaniya chegarasi oʻtadi. Bu joyda balandligi 1424 m boʻlgan Vogez togʻ tizmasi mavjud. Fransiya va Ispaniya oʻrtasida tabiiy chegara boʻlgan Pireney togʻlarining balandligi Fransiya tomonida 3298 m ga (Vinmal) yetadi. Fransiyada foydali qazilmalardan koʻmir, temir rudasi, uran, tabiiy gaz, volfram va surma, talk xomashyosi, qoʻrgʻoshin, oltingugurt va rux bor. Neft va gaz juda kam miqdorda.` }
+];
+
+function showRegister() {
+    loginBox.classList.add("hidden");
+    registerBox.classList.remove("hidden");
+}
+
+function showLogin() {
+    registerBox.classList.add("hidden");
+    loginBox.classList.remove("hidden");
+}
+
+function register(e) {
+    e.preventDefault();
+    const user = {
+        name: regName.value,
+        surname: regSurname.value,
+        password: regPassword.value
+    };
+    localStorage.setItem("user", JSON.stringify(user));
+    alert("Registered!");
+    showLogin();
+}
+
+function login(e) {
+    e.preventDefault();
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (
+        user &&
+        loginName.value === user.name &&
+        loginSurname.value === user.surname &&
+        loginPassword.value === user.password
+    ) {
+        loginBox.classList.add("hidden");
+        mainBox.classList.remove("hidden");
+        userInfo.innerText = user.name + " " + user.surname;
+    } else {
+        alert("Xato ma'lumot");
+    }
+}
+
+function start() {
+    asosiy.classList.remove("hidden");
+    asosi.classList.add("hidden");
+    mainBox.classList.add('hidden')
+    
+}
+
+function openDavlatlar() {
+    davlatlarBox.classList.remove("hidden");
+    imghidden.classList.add('hidden');
+
+    render(davlatlar);
+}
+
+function backMenu() {
+    davlatlarBox.classList.add("hidden");
+    info.innerHTML = "";
+    search.value = "";
+}
+
+function render(arr) {
+    list.innerHTML = "";
+    arr.forEach(d => {
+        const li = document.createElement("li");
+        li.innerText = d.name;
+        li.onclick = () => info.innerHTML = `<h3>${d.name}</h3><p>${d.info}</p>`;
+        list.appendChild(li);
+    });
+}
+
+function filterDavlat() {
+    const val = search.value.toLowerCase();
+    render(davlatlar.filter(d => d.name.toLowerCase().includes(val)));
+}
+
+function Futbolchi_f() {
+    imghidden.classList.remove('hidden');
+    asosiy.classList.add("hidden");
+    davlatlarBox.classList.add('hidden');
+}
